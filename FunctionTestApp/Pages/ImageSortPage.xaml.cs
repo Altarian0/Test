ee5702977546d5b7f5e97e02c3f2826223776663
Gogs <gogs@fake.local>
2020-03-18 16:59:37 +0300

    Test

--- FunctionTestApp/Pages/ImageSortPage.xaml.cs
+++ FunctionTestApp/Pages/ImageSortPage.xaml.cs
@@ -2,19 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FunctionTestApp.Pages
 {
@@ -23,11 +15,14 @@ namespace FunctionTestApp.Pages
     /// </summary>
     public partial class ImageSortPage : Page
     {
+        string Username;
         FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
         List<SortImage> sortImages = new List<SortImage>();
         public ImageSortPage()
         {
             InitializeComponent();
+
+            Username = Environment.UserName;
         }
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
@@ -68,30 +63,43 @@ namespace FunctionTestApp.Pages
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
-            SortListBox.Items.Clear();
-            sortImages.Clear();
-
-            DirectoryInfo directoryInfo = new DirectoryInfo(PathLabel.Text);
-            var allFoundFiles = directoryInfo.GetFiles(@"*.jpg", SearchOption.AllDirectories);
-            var allFoundFilesPng = directoryInfo.GetFiles(@"*.png", SearchOption.AllDirectories);
-
-            foreach (var file in allFoundFiles)
+            if (PathLabel.Text != "")
             {
-                sortImages.Add(new SortImage { FileInfo = file });
-            }
+                DirectoryInfo directoryInfo = null;
+                SortListBox.Items.Clear();
+                sortImages.Clear();
+                try
+                {
+                    directoryInfo = new DirectoryInfo(PathLabel.Text);
+                    var allFoundFiles = directoryInfo.GetFiles(@"*.jpg", SearchOption.AllDirectories);
+                    var allFoundFilesPng = directoryInfo.GetFiles(@"*.png", SearchOption.AllDirectories);
+                    foreach (var file in allFoundFiles)
+                    {
+                        sortImages.Add(new SortImage { FileInfo = file });
+                    }
 
-            foreach (var file in allFoundFilesPng)
+                    foreach (var file in allFoundFilesPng)
+                    {
+                        sortImages.Add(new SortImage { FileInfo = file });
+                    }
+
+                    foreach (var file in sortImages)
+                    {
+                        SortListBox.Items.Add("Имя: " + file.FileInfo.Name + ". Размер: " + Math.Round(file.FileInfo.Length / 1024.0 / 1024.0, 2) + " Мб.");
+                    }
+                }
+                catch
+                {
+                    System.Windows.MessageBox.Show("Неверный путь!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+                ClearButton.IsEnabled = true;
+                SortingButton.IsEnabled = true;
+            }
+            else
             {
-                sortImages.Add(new SortImage { FileInfo = file });
+                System.Windows.MessageBox.Show("Папка не выбрана", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
-
-            foreach (var file in sortImages)
-            {
-                SortListBox.Items.Add("Имя: " + file.FileInfo.Name + ". Размер: " + Math.Round(file.FileInfo.Length / 1024.0 / 1024.0, 2) + " Мб.");
-            }
-
-            ClearButton.IsEnabled = true;
-            SortingButton.IsEnabled = true;
         }
 
         private void SortingButton_Click(object sender, RoutedEventArgs e)
@@ -105,14 +113,14 @@ namespace FunctionTestApp.Pages
                 {
                     foreach (var item in sortImages)
                     {
-                        
-                        
+
+
                         if (item.FileInfo.CreationTime.Day == Day)
                         {
                             try
                             {
 
-                            item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + item.FileInfo.CreationTime.ToString("dd.MM.yyyy") + @"\" + item.FileInfo.Name);
+                                item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + item.FileInfo.CreationTime.ToString("dd.MM.yyyy") + @"\" + item.FileInfo.Name);
                             }
                             catch
                             {
@@ -127,13 +135,13 @@ namespace FunctionTestApp.Pages
                             if (!newFolder.Exists)
                             {
                                 newFolder.Create();
-                                item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + folderName + @"\" + item.FileInfo.Name);
+                                item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + folderName + @"\" + item.FileInfo.Name);
                             }
                             else
                             {
                                 try
                                 {
-                                    item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + folderName + @"\" + item.FileInfo.Name);
+                                    item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + folderName + @"\" + item.FileInfo.Name);
                                 }
                                 catch
                                 {
@@ -151,8 +159,15 @@ namespace FunctionTestApp.Pages
 
                         if (item.FileInfo.CreationTime.Month == Month)
                         {
+                            try
+                            {
 
-                            item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + item.FileInfo.CreationTime.ToString("MM.yyyy") + @"\" + item.FileInfo.Name);
+                            item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + item.FileInfo.CreationTime.ToString("MM.yyyy") + @"\" + item.FileInfo.Name);
+                            }
+                            catch
+                            {
+
+                            }
                         }
                         else
                         {
@@ -162,13 +177,13 @@ namespace FunctionTestApp.Pages
                             if (!newFolder.Exists)
                             {
                                 newFolder.Create();
-                                item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + folderName + @"\" + item.FileInfo.Name);
+                                item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + folderName + @"\" + item.FileInfo.Name);
                             }
                             else
                             {
                                 try
                                 {
-                                    item.FileInfo.CopyTo(@"C:\Users\36\Desktop\" + folderName + @"\" + item.FileInfo.Name);
+                                    item.FileInfo.CopyTo(@"C:\Users\" + Username + @"\Desktop\" + folderName + @"\" + item.FileInfo.Name);
                                 }
                                 catch
                                 {
@@ -189,7 +204,7 @@ namespace FunctionTestApp.Pages
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Image files (*.jpg)|*.jpg";
-            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)this.MainGrid.Width, (int)this.MainGrid.Height, 96, 96, PixelFormats.Pbgra32);
                 renderTargetBitmap.Render(MainGrid);
